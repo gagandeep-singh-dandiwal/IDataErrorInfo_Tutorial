2a3584299e74e87ceb5069cf7c2ebff0bab2aadb
gagandeep901 <gagandeep901@gmail.com>
2024-01-21 20:29:33 +0530

    Add project files.

---
+++ IDataErrorInfo_Tutorial/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDataErrorInfo_Tutorial
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            Username = string.Empty;
+            Password = string.Empty;
+        }
+        private string username;
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                if (string.IsNullOrEmpty(username))
+                {
+                    AddError(nameof(Username), "Cannot be empty");
+                
+                }
+                else
+                {
+                    RemoveError(nameof(Username), "Cannot be empty");
+                }
+                if (username?.Length < 3)
+                {
+                    AddError(nameof(Username), "Length cannot be less than 3");
+                }
+                else
+                {
+                    RemoveError(nameof(Username), "Length cannot be less than 3");
+                }
+                if (username?.Length > 10)
+                {
+                    AddError(nameof(Username), "Length cannot be more than 10");
+                }
+                else
+                {
+                    RemoveError(nameof(Username), "Length cannot be more than 10");
+                }
+                RaisepropertyChanged();
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                RaisepropertyChanged();
+            }
+        }
+    }
+}
---
+++ IDataErrorInfo_Tutorial/ViewModelBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDataErrorInfo_Tutorial
+{
+    internal class ViewModelBase : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public Dictionary<string, ObservableCollection<string>> ErrorsInThisViewModel { get; set; } = new Dictionary<string, ObservableCollection<string>>();
+
+        public string Error
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                ErrorsInThisViewModel.TryGetValue(columnName, out var value);
+                if (value != null && value.Any())
+                {
+                    return value[0];
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+
+        public void AddError(string propertyName, string errorMessage)
+        {
+            //if the error already exist in ErrosInThisViewModel
+            ErrorsInThisViewModel.TryGetValue(propertyName, out var observable);
+
+            //if the property does not exist add it
+            if (observable == null || observable.Count == 0)
+            {
+                ErrorsInThisViewModel.Add(propertyName, new ObservableCollection<string>());
+                ErrorsInThisViewModel[propertyName].Add(errorMessage);
+            }
+
+            //if the property already exists , add the error message to the list of errors
+            else if (observable != null && !observable.Contains(errorMessage))
+            {
+                observable.Add(errorMessage);
+            }
+        }
+
+        public void RemoveError(string propertyName, string errorMessage)
+        {
+            ErrorsInThisViewModel.TryGetValue(propertyName, out var observable);
+
+            if (observable != null && observable.Contains(errorMessage) && observable.Count == 1)
+            {
+                ErrorsInThisViewModel.Remove(propertyName);
+            }
+            else if(observable != null && observable.Contains(errorMessage))
+            {
+                observable.Remove(errorMessage);
+            }
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisepropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
